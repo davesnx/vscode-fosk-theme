971dd33187311b240c0d7eea8604fa91de7d7734
David Sancho Moreno <dsnxmoreno@gmail.com>
2024-11-04 21:09:58 +0100

    Push v0

--- src/build.ts
+++ src/build.ts
@@ -1,7 +1,13 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import template from './template'
+/* import * as Fs from 'fs'
+import * as Path from 'path'
+import fosk from './fosk'
 
-const content = JSON.stringify(template(), null, '\t')
-fs.writeFileSync(path.join(process.cwd(), `/fosk.json`), content)
-console.log(`Generate theme`)
+const fosk_content = JSON.stringify(fosk(), null, '\t')
+Fs.writeFileSync(Path.join(process.cwd(), `/fosk.json`), fosk_content)
+console.log(`Generate fosk theme`)
+
+import klar from './klar'
+const klar_content = JSON.stringify(klar(), null, '\t')
+Fs.writeFileSync(Path.join(process.cwd(), `/klar.json`), klar_content)
+console.log(`Generate klar theme`)
+*/
